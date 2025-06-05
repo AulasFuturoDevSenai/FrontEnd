0b8fc8374141a4ea011ebe62a2b4462a6566f368
Emanuele Magalhães <emanuele.lms@gmail.com>
2025-06-04 22:03:01 -0300

    Exemplos de switc case

---
+++ SEMANA3/aula/quar/switch1.js
@@ -0,0 +1,31 @@
+let numero = parseInt(prompt('Digite o um número: '));
+
+switch (numero) {
+    case 1:
+        console.log('Domingo');
+        break;
+    case 2:
+        console.log('Segunda-feira');
+        break;
+    case 2:
+        console.log('Segunda-feira');
+        break;
+    case 3:
+        console.log('terça-feira');
+        break;
+    case 4:
+        console.log('quarta-feira');
+        break;
+    case 5:
+        console.log('quinta-feira');
+        break;
+    case 6:
+        console.log('sexta-feira');
+        break;
+        case 7:
+    console.log('sabado');
+        break;
+    default:
+        console.error('Número invalido')
+        break;
+}
