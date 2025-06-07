85aaa9e05ff73143eee285cc562a5094aefb229b
Emanuele Magalhães <emanuele.lms@gmail.com>
2025-06-07 11:46:21 -0300

    switch case para calcular frete

--- SEMANA3/trello/atividade4.js
+++ SEMANA3/trello/atividade4.js
@@ -1,9 +1,35 @@
+//calcular o frete de um produto
+
 const regiao = prompt('Digite a região desejada: ');
 const peso = parseFloat(prompt('Digite o peso do produto em kg: '));
+const regiaoFormatada = regiao.trim().toUpperCase();
 
-let norte = 24 + (2,20 * peso);
+let valorFrete;
 
-if (regiao.toLocaleUpperCase == 'NORTE') {
-    console.log(norte.toFixed(2));
+
+switch (regiaoFormatada) {
+    case 'NORTE':
+        valorFrete = 24 + (2.20 * peso);
+        break;
+    case 'NORDESTE':
+        valorFrete = 22 + (2.10 * peso);
+        break;
+    case 'CENTRO-OESTE':
+        valorFrete = 20 + (2.00 * peso);
+        break;
+    case 'SUDESTE':
+        valorFrete = 16 + (1.80 * peso);
+        break;    
+    case 'SUL':
+        valorFrete = 18 + (1.90 * peso);
+        break;
+
+    default:
+        console.log('Região inválida. Por favor, tente novamente.');
+        break;
 }
 
+//Se a região for válida, exibe o valor do frete
+if (valorFrete !== undefined) {
+    console.log(`O valor do frete para a região ${regiaoFormatada} é R$ ${valorFrete.toFixed(2)}`);
+}
